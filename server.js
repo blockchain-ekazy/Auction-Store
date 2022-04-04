e1843ab3aca45a8e917d073ddd55b33751740322
Sabirpro <blockchain.ekazy@gmail.com>
2022-04-04 11:23:46 +0500

    fix again

--- server.js
+++ server.js
@@ -28,11 +28,6 @@ app.use(express.static(path.resolve(__dirname, "./public")));
 //metadata api
 app.use("/api/", require("./src/api/metadata"));
 
-// serve default
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "build", "index.html"));
-});
-
 //image upload
 app.post("/upload", function (req, res) {
   upload(req, res, function (err) {
@@ -43,6 +38,11 @@ app.post("/upload", function (req, res) {
     }
     return res.status(200).send(req.file);
   });
+});
+
+// serve default
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
 });
 
 app.listen(process.env.PORT || 8000, function () {
